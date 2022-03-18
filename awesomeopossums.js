54459328128ce44424e1a2555490fcb8ea623f7f
Stephanie Boggs <briteoctopus@gmail.com>
2022-03-17 21:06:09 -0400

    pic fix, JS for form half done

--- awesomeopossums.js
+++ awesomeopossums.js
@@ -1,3 +1,4 @@
+//hamburger code start
 const button = document.getElementById('hamburger');
 
 button.addEventListener('click', function(){
@@ -6,3 +7,11 @@ button.addEventListener('click', function(){
         navlinks.classList.remove('invis');}
         else {navlinks.classList.add('invis');}
     })
+//hamburger code end
+
+//form alert code start
+function formGrab() {
+    const name = document.getElementById("submitform").elements[0].value;
+    alert("Thank you " + name + ", your submission has been receieved!");
+}
+//form alert code end
